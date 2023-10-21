cfff43d2df592417bc2d8cbb7af049056bbfce84
Ritik_Kumar_Pathak <111448294+hritikpathak06@users.noreply.github.com>
2023-10-21 18:48:37 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,13 @@ const path = require("path");
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://deploy-mern-1whq.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"../frontend/build")))
